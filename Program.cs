647863f00c78360205972c72fbc2b1fcbb56f721
Garnold-Crump-Student <gacr846@student.oaisd.org>
2024-10-07 13:03:58 +0000

    Main

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My Name is Garnold Crump");
+
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Garnold";
+        string lastname = "Crump";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
